a69125a7f3f133675928a5434024daef6c581e21
Daniel Fuka <drfuka@vt.edu>
2023-02-16 14:45:48 -0500

    Fixed Snowmelt

--- R/TISnow.R
+++ R/TISnow.R
@@ -5,12 +5,12 @@
 #Tlag = 1  # referred to as TIMP in SWAT input (Table 1)
 
 TISnow=function(WBData,SFTmp=2,bmlt6=4.5,bmlt12=0.0,Tmlt=3,Tlag=1){
-  #  WBData=TMWB
-  #  SFTmp = 3  # referred to as SFTMP in SWAT input (Table 1)
-  #  bmlt6 = 4.5   # referred to as SMFMX in SWAT input (Table 1)
-  #  bmlt12 = 0.0  # referred to as SMFMN in SWAT input adjusted for season
-  #  Tmlt = SFTmp  # Assumed to be same as SnowFall Temperature
-  #  Tlag = 1  # referred to as TIMP in SWAT input (Table 1)
+#WBData=TMWB
+#SFTmp = 3  # referred to as SFTMP in SWAT input (Table 1)
+#bmlt6 = 4.5   # referred to as SMFMX in SWAT input (Table 1)
+#bmlt12 = 0.0  # referred to as SMFMN in SWAT input adjusted for season
+#Tmlt = SFTmp  # Assumed to be same as SnowFall Temperature
+#Tlag = 1  # referred to as TIMP in SWAT input (Table 1)
   WBData$AvgTemp=(WBData$MaxTemp-WBData$MinTemp)/2
   WBData$bmlt = (bmlt6 + bmlt12)/2 + (bmlt6 - bmlt12)/2 * 
     sin(2*pi/365*(julian(WBData$date,origin = as.Date("2000-01-01"))-81))
@@ -28,7 +28,7 @@ TISnow=function(WBData,SFTmp=2,bmlt6=4.5,bmlt12=0.0,Tmlt=3,Tlag=1){
       #
       # Eeee... I forgot to save my snowfall!
       #
-      SNOfall=P[t]
+      SNOfall[t]=P[t]
     }  else {
       SNOmlt[t]= bmlt[t] * SNO[t-1] * ((Tsno[t]+MaxTemp[t])/2 - Tmlt) 
       SNOmlt[t]= min(SNOmlt[t],SNO[t-1])
@@ -41,7 +41,7 @@ TISnow=function(WBData,SFTmp=2,bmlt6=4.5,bmlt12=0.0,Tmlt=3,Tlag=1){
   WBData$Tsno=Tsno
   WBData$SNO=SNO
   WBData$SNOmlt=SNOmlt
-  WBData$SNOmlt=SNOfall
+  WBData$SNOfall=SNOfall
   rm(list=c("SNO", "SNOmlt", "Tsno","SNOfall"))
   return(data.frame(Tsno=WBData$Tsno,SNO=WBData$SNO,SNOmlt=WBData$SNOmlt,SNOfall=WBData$SNOfall))
 }
